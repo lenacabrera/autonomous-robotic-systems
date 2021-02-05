94bbc984d77358a4ceee849c05722c4ee1e23aa5
kathrin229 <39700827+kathrin229@users.noreply.github.com>
2021-02-05 17:17:02 +0100

    some parameter changes, different use of maximum velocity

--- particle.py
+++ particle.py
@@ -2,6 +2,7 @@
 # update velocity and position for one iteration
 import random
 
+import numpy
 from pygame import *
 
 import benchmark_functions
@@ -25,16 +26,20 @@ class Particle:
         draw.circle(screen, self.colour, (self.p[0], self.p[1]), self.size, self.thickness)
 
 
-    def update(self, gbest, a, b, c, delta_t):
+    def update(self, gbest, a, b, c, delta_t, v_max):
         # update velocity and position for one iteration
         r1 = random.uniform(0, 1)
         r2 = random.uniform(0, 1)
         v_new = a * self.v + b * r1 * (self.pbest - self.p) + c * r2 * (gbest - self.p)
         p_new = self.p + self.v * delta_t
-        if (v_new[0] > -50) and (v_new[1] > -50) and v_new[0] < 50 and v_new[1] < 50:
+
+        if numpy.sqrt((v_new**2).sum(axis=0)) <= v_max:
             self.v = v_new
-        if (p_new[0] > 0) and (p_new[1] > 0) and p_new[0] < 200 and p_new[1] < 200:
-            self.p = p_new
+            print("change v ", v_new)
+            if p_new[0] > 0 and p_new[1] > 0 and p_new[0] < 200 and p_new[1] < 200:
+                self.p = p_new
+                print("change p ", p_new)
+
 
     def evaluate(self, b_func):
         # evaluate performance/fitness of a position
--- pso.py
+++ pso.py
@@ -2,33 +2,45 @@ from particle import Particle
 import numpy
 import random
 from pygame import *
+import benchmark_functions
 
 def pso(n_particles, n_iterations, benchmark_function):
 
-    init_velocity = numpy.array([2, 2]) #zeros(2)  # angle and distance  TODO
+    init_velocity = numpy.array([0, 0]) #zeros(2)  # angle and distance  TODO
 
     # initialize all particles
     particles = []
+    gbest = numpy.array([-1, -1])
+    gbest_fitness = -1
+
     for i_particle in range(n_particles):
         x_rand = random.uniform(0, 200)  # TODO
         y_rand = random.uniform(0, 200)  # TODO
         init_position = numpy.array([x_rand, y_rand])
-        particles.append(Particle(init_velocity, init_position))
+        particle = Particle(init_velocity, init_position)
 
-    gbest = numpy.array([-1, -1])
-    gbest_fitness = -1
+        if benchmark_function == 'rosenbrock':
+            p_fitness = benchmark_functions.rosenbrock(particle.p)
+
+        if benchmark_function == 'rastrigin':
+            p_fitness = benchmark_functions.rastrigin(particle.p)
+
+        if p_fitness > gbest_fitness:
+            gbest_fitness = p_fitness
+            gbest = particle.p
+
+        particles.append(particle)
+
     a = 0.9
     b = 2
     c = 2
     delta_t = 1
+    v_max = 30
 
     screen = display.set_mode((200, 200))
     # Fill screen white
     screen.fill((255, 255, 255))
 
-    # Draw a red circle at the center
-    # Remove the 1 to make it a filled circle
-    draw.circle(screen, (255, 0, 0), (250, 250), 250, 1)
 
     def waitFor(waitTime):  # waitTime in milliseconds
         screenCopy = screen.copy()
@@ -52,15 +64,18 @@ def pso(n_particles, n_iterations, benchmark_function):
         # update
         screen.fill((255, 255, 255))
         for particle in particles:
-            particle.update(gbest, a, b, c, delta_t)
+            particle.update(gbest, a, b, c, delta_t, v_max)
             particle.evaluate(benchmark_function)
             particle.display(screen)
 
             if particle.pbest_fitness > gbest_fitness:
                 gbest_fitness = particle.pbest_fitness
                 gbest = particle.pbest
-        n_iterations= n_iterations-1
+
+        n_iterations = n_iterations-1
+
         print(n_iterations)
+
         if n_iterations == 1:
             running = False
             for particle in particles:
@@ -84,4 +99,4 @@ def pso(n_particles, n_iterations, benchmark_function):
 if __name__ == '__main__':
 
 
-    pso(20, 100, 'rosenbrock')
+    pso(20, 100, 'rastrigin')
