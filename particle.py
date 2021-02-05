782548bd1bb5306bc32a7604c62ee4a83b05f03f
kathrin229 <39700827+kathrin229@users.noreply.github.com>
2021-02-05 15:06:48 +0100

    plot with pygame, boundaries for velocity and position, some parameter changes

--- particle.py
+++ particle.py
@@ -1,6 +1,9 @@
 # import pso
 # update velocity and position for one iteration
 import random
+
+from pygame import *
+
 import benchmark_functions
 
 
@@ -14,14 +17,24 @@ class Particle:
         self.pos_history = []
         # self.performance = performance  # benchmark functions?
 
+        self.size = 5
+        self.colour = (0, 0, 255)
+        self.thickness = 5
+
+    def display(self, screen):
+        draw.circle(screen, self.colour, (self.p[0], self.p[1]), self.size, self.thickness)
+
+
     def update(self, gbest, a, b, c, delta_t):
         # update velocity and position for one iteration
         r1 = random.uniform(0, 1)
         r2 = random.uniform(0, 1)
         v_new = a * self.v + b * r1 * (self.pbest - self.p) + c * r2 * (gbest - self.p)
         p_new = self.p + self.v * delta_t
-        self.v = v_new
-        self.p = p_new
+        if (v_new[0] > -50) and (v_new[1] > -50) and v_new[0] < 50 and v_new[1] < 50:
+            self.v = v_new
+        if (p_new[0] > 0) and (p_new[1] > 0) and p_new[0] < 200 and p_new[1] < 200:
+            self.p = p_new
 
     def evaluate(self, b_func):
         # evaluate performance/fitness of a position
--- pso.py
+++ pso.py
@@ -1,16 +1,17 @@
 from particle import Particle
 import numpy
 import random
+from pygame import *
 
 def pso(n_particles, n_iterations, benchmark_function):
 
-    init_velocity = numpy.zeros(2)  # angle and distance  TODO
+    init_velocity = numpy.array([2, 2]) #zeros(2)  # angle and distance  TODO
 
     # initialize all particles
     particles = []
     for i_particle in range(n_particles):
-        x_rand = random.uniform(0, 10)  # TODO
-        y_rand = random.uniform(0, 10)  # TODO
+        x_rand = random.uniform(0, 200)  # TODO
+        y_rand = random.uniform(0, 200)  # TODO
         init_position = numpy.array([x_rand, y_rand])
         particles.append(Particle(init_velocity, init_position))
 
@@ -21,22 +22,66 @@ def pso(n_particles, n_iterations, benchmark_function):
     c = 2
     delta_t = 1
 
-    # update
-    for iteration in range(n_iterations):
+    screen = display.set_mode((200, 200))
+    # Fill screen white
+    screen.fill((255, 255, 255))
 
+    # Draw a red circle at the center
+    # Remove the 1 to make it a filled circle
+    draw.circle(screen, (255, 0, 0), (250, 250), 250, 1)
+
+    def waitFor(waitTime):  # waitTime in milliseconds
+        screenCopy = screen.copy()
+        waitCount = 0
+        while waitCount < waitTime:
+            clock = time.Clock()
+            dt = clock.tick(60)  # 60 is your FPS here
+            waitCount += dt
+            event.pump()  # Tells pygame to handle it's event, instead of pygame.event.get()
+            screen.blit(screenCopy, (0, 0))
+            display.flip()
+
+    # Game loop: idea from: https://www.petercollingridge.co.uk/tutorials/pygame-physics-simulation/
+    running = True
+    while running:
+        # Properly quit (pygame will crash without this)
+        for e in event.get():
+            if e.type == QUIT:
+                running = False
+
+        # update
+        screen.fill((255, 255, 255))
         for particle in particles:
             particle.update(gbest, a, b, c, delta_t)
             particle.evaluate(benchmark_function)
+            particle.display(screen)
 
             if particle.pbest_fitness > gbest_fitness:
                 gbest_fitness = particle.pbest_fitness
                 gbest = particle.pbest
+        n_iterations= n_iterations-1
+        print(n_iterations)
+        if n_iterations == 1:
+            running = False
+            for particle in particles:
+                print(particle.p)
 
         # decrease a
         decrease_amount = (0.9 - 0.4) / n_iterations
         a -= decrease_amount
+        waitFor(500)
+
+
+
+        display.flip()
+    quit()
+
+
+
+
 
 
 if __name__ == '__main__':
 
-    pso(5, 5, 'rosenbrock')
+
+    pso(20, 100, 'rosenbrock')
