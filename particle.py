47b03dd06f2d097dfaa509976e525c0f7b316324
Lena Cabrera <lena.m.cabrera@gmail.com>
2021-02-07 18:02:09 +0100

    Addes different strategies for particles moving out of search space.

--- particle.py
+++ particle.py
@@ -25,26 +25,94 @@ class Particle:
     def display(self, screen, frame_range):
         draw.circle(screen, self.colour, (self.p[0] + frame_range[1], self.p[1] + frame_range[1]), self.size, self.thickness)
 
-
-    def update(self, gbest, a, b, c, delta_t, v_max, frame_range):
+    def update(self, gbest, a, b, c, r_max, delta_t, v_max, frame_range, oof_strategy):
         # update velocity and position for one iteration
-        r1 = random.uniform(0, 1)
-        r2 = random.uniform(0, 1)
+        r1 = random.uniform(0, r_max)
+        r2 = random.uniform(0, r_max)
         v_new = a * self.v + b * r1 * (self.pbest - self.p) + c * r2 * (gbest - self.p)
-        p_new = self.p + self.v * delta_t
 
-        # what happen if particle move out of frame/search space? # TODO (add parameter)
-        # 1. stick with old position
-        if numpy.sqrt((v_new**2).sum(axis=0)) <= v_max:
-            self.v = v_new
-            print("change v ", v_new)
-            if p_new[0] > frame_range[0] and p_new[1] > frame_range[0] and p_new[0] < frame_range[1] and p_new[1] < frame_range[1]:
-                self.p = p_new
-                print("change p ", p_new)
-        # 2. move in opposite direction (x and/or y)
+        distance = numpy.sqrt((v_new**2).sum(axis=0))
+        if distance > v_max:
+            # v_new == v_max
+            # keep old velocity
+            v_new = self.v
+            print("distance greater than v_max")
 
-        # 3. new velocity with new random r1 and r2 (x and/or y)
+        p_new = self.p + v_new * delta_t
+        # p_new = self.p + self.v * delta_t
 
+        # check if new position is outside of frame
+        x_is_oof = p_new[0] < frame_range[0] or p_new[0] > frame_range[1]
+        y_is_oof = p_new[1] < frame_range[0] or p_new[1] > frame_range[1]
+
+        if x_is_oof or y_is_oof:
+
+            x_counter = 2  # try strategy max. 2 times because some can again lead to out of frame behavior...
+            while x_is_oof and x_counter > 0:
+                # x is out of frame
+                if oof_strategy == 0:
+                    # keep old position and velocity
+                    return
+                elif oof_strategy == 1:
+                    # move in opposite direction
+                    xv_new = - v_new[0]
+                    v_new[0] = xv_new
+                elif oof_strategy == 2:
+                    # new random r1 and r2
+                    r1 = random.uniform(0, 1)
+                    r2 = random.uniform(0, 1)
+                    xv_new = a * self.v[0] + b * r1 * (self.pbest[0] - self.p[0]) + c * r2 * (gbest[0] - self.p[0])
+                    v_new[0] = xv_new
+                elif oof_strategy == 3:
+                    # make much smaller step in new direction
+                    v_new[0] = 0.1 * v_new[0]
+                elif oof_strategy == 4:
+                    # keep old x
+                    v_new[0] = self.v[0]
+
+                p_new = self.p + v_new * delta_t
+                x_is_oof = p_new[0] < frame_range[0] or p_new[0] > frame_range[1]
+                x_counter -= 1
+
+            if x_counter == 0:
+                # keep old position and velocity
+                return
+
+            y_counter = 2  # try strategy max. 2 times because some can again lead to out of frame behavior...
+            while y_is_oof and y_counter > 0:
+                # y is out of frame
+                if oof_strategy == 0:
+                    # keep old position
+                    return
+                elif oof_strategy == 1:
+                    # move in opposite direction
+                    yv_new = - v_new[1]
+                    v_new[1] = yv_new
+                elif oof_strategy == 2:
+                    # new random r1 and r2
+                    r1 = random.uniform(0, 1)
+                    r2 = random.uniform(0, 1)
+                    yv_new = a * self.v[1] + b * r1 * (self.pbest[1] - self.p[1]) + c * r2 * (gbest[1] - self.p[1])
+                    v_new[1] = yv_new
+                elif oof_strategy == 3:
+                    # make much smaller step in new direction
+                    v_new[1] = 0.1 * v_new[1]
+                elif oof_strategy == 4:
+                    # keep old y
+                    v_new[1] = self.v[1]
+
+                p_new = self.p + v_new * delta_t
+                y_is_oof = p_new[1] < frame_range[0] or p_new[1] > frame_range[1]
+                y_counter -= 1
+
+            if y_counter == 0:
+                # keep old position and velocity
+                return
+
+        # update current velocity and position
+        self.v = v_new
+        self.p = p_new
+        print(self.p)
 
     def evaluate(self, b_func):
         # evaluate performance/fitness of a position
--- pso.py
+++ pso.py
@@ -4,11 +4,15 @@ import random
 from pygame import *
 import benchmark_functions
 
-def pso(n_particles, n_iterations, benchmark_function, a, b, c, delta_t, v_max, frame_range):
 
-    # set velocity to zero
-    init_velocity = numpy.array([0, 0])
-    # set velocity to random value TODO add parameter
+def pso(n_particles, n_iterations, benchmark_function, a, b, c, r_max, delta_t, v_max, frame_range, random_init_v,
+        oof_strategy):
+    if random_init_v:
+        # set velocity to random value
+        init_velocity = numpy.array([random.uniform(-v_max, v_max), random.uniform(-v_max, v_max)])
+    else:
+        # set velocity to zero
+        init_velocity = numpy.array([0, 0])
 
     # initialize all particles
     particles = []
@@ -19,6 +23,7 @@ def pso(n_particles, n_iterations, benchmark_function, a, b, c, delta_t, v_max, 
         x_rand = random.uniform(frame_range[0], frame_range[1])
         y_rand = random.uniform(frame_range[0], frame_range[1])
         init_position = numpy.array([x_rand, y_rand])
+        print(init_position)
         particle = Particle(init_velocity, init_position)
 
         if benchmark_function == 'rosenbrock':
@@ -33,13 +38,10 @@ def pso(n_particles, n_iterations, benchmark_function, a, b, c, delta_t, v_max, 
 
         particles.append(particle)
 
-
-
     # TODO look at other
-    screen = display.set_mode((2*frame_range[1], 2*frame_range[1]))
+    screen = display.set_mode((2 * frame_range[1], 2 * frame_range[1]))
     # Fill screen white
     screen.fill((255, 255, 255))
-
 
     def waitFor(waitTime):  # waitTime in milliseconds
         screenCopy = screen.copy()
@@ -63,7 +65,7 @@ def pso(n_particles, n_iterations, benchmark_function, a, b, c, delta_t, v_max, 
         # update
         screen.fill((255, 255, 255))
         for particle in particles:
-            particle.update(gbest, a, b, c, delta_t, v_max, frame_range)
+            particle.update(gbest, a, b, c, r_max, delta_t, v_max, frame_range, oof_strategy)
             particle.evaluate(benchmark_function)
             particle.display(screen, frame_range)
 
@@ -71,8 +73,7 @@ def pso(n_particles, n_iterations, benchmark_function, a, b, c, delta_t, v_max, 
                 gbest_fitness = particle.pbest_fitness
                 gbest = particle.pbest
 
-        n_iterations = n_iterations-1
-
+        n_iterations = n_iterations - 1
         print(n_iterations)
 
         if n_iterations == 1:
@@ -83,28 +84,31 @@ def pso(n_particles, n_iterations, benchmark_function, a, b, c, delta_t, v_max, 
         # decrease a
         decrease_amount = (0.9 - 0.4) / n_iterations
         a -= decrease_amount
+
         waitFor(500)
-
-
-
         display.flip()
     quit()
 
 
-
-
-
-
 if __name__ == '__main__':
-
-
     pso(n_particles=20,
         n_iterations=100,
         benchmark_function='rastrigin',
         a=0.9,
         b=2,
         c=2,
+        r_max=1,
         delta_t=1,
         v_max=30,
-        frame_range=[-100,100],
-    )
+        frame_range=[-100, 100],
+        random_init_v=False,
+        # "out of screen strategy"
+        # 0=old position,
+        # 1=change direction,
+        # 2=new random (r1, r2),
+        # 3=only small step in new direction,
+        # 4=old coordinate
+        oof_strategy=3
+        )
+
+    exit()
