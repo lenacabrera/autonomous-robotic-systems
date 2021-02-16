8bb6bff4b437de2949551bae4a85d7dcfe8829a4
Lena Cabrera <lena.m.cabrera@gmail.com>
2021-02-16 15:04:49 +0100

    implementation of sensor model

--- 2_mobile_robot_simulator/main.py
+++ 2_mobile_robot_simulator/main.py
@@ -3,39 +3,69 @@ import sys
 from robot import Robot
 
 
-def draw_walls(screen, env_width, env_height, wall_length, wall_thickness, wall_color):
+
+def draw_walls(screen, walls, wall_thickness, wall_color):
+
+    # left
+    pygame.draw.line(surface=screen, color=wall_color, width=wall_thickness,
+                     start_pos=walls['left'][0],
+                     end_pos=walls['left'][1])
+
+    # top
+    pygame.draw.line(surface=screen, color=wall_color, width=wall_thickness,
+                     start_pos=walls['top'][0],
+                     end_pos=walls['top'][1])
+    # right
+    pygame.draw.line(surface=screen, color=wall_color, width=wall_thickness,
+                     start_pos=walls['right'][0],
+                     end_pos=walls['right'][1])
+
+    # bottom
+    pygame.draw.line(surface=screen, color=wall_color, width=wall_thickness,
+                     start_pos=walls['bottom'][0],
+                     end_pos=walls['bottom'][1])
+
+
+def draw_robot(screen, robot, robot_color, draw_sensors=False):
+    # body of robot
+    pygame.draw.circle(surface=screen, color=robot_color, center=(robot.x, robot.y), radius=robot.radius)
+    # orientation of robot
+    pygame.draw.line(surface=screen, color=(0, 0, 0), width=2,
+                     start_pos=(robot.x, robot.y), end_pos=(robot.x + robot.radius, robot.y))
+    # sensors
+    if draw_sensors:
+        for (x_sensor, y_sensor) in robot.sensor_list:
+            pygame.draw.line(surface=screen, color=(0, 0, 0), width=2,
+                             start_pos=(robot.x, robot.y), end_pos=(x_sensor, y_sensor))
+
+
+def init_walls_coordinates(env_width, env_height, wall_length):
 
     # wall frame distance
     dist_l_r = (env_width - wall_length) / 2  # distance to frame, left and right
     dist_t_b = (env_height - wall_length) / 2  # distance to frame, top and bottom
 
-    # left
-    pygame.draw.line(surface=screen, color=wall_color, width=wall_thickness,
-                     start_pos=(dist_l_r, dist_t_b),
-                     end_pos=(dist_l_r, dist_t_b + wall_length))
 
-    # top
-    pygame.draw.line(surface=screen, color=wall_color, width=wall_thickness,
-                     start_pos=(dist_l_r, dist_t_b + wall_length),
-                     end_pos=(dist_l_r + wall_length, dist_t_b + wall_length))
-    # right
-    pygame.draw.line(surface=screen, color=wall_color, width=wall_thickness,
-                     start_pos=(dist_l_r + wall_length, dist_t_b + wall_length),
-                     end_pos=(dist_l_r + wall_length, dist_t_b))
+    left_start = (dist_l_r, dist_t_b)
+    left_end = (dist_l_r, dist_t_b + wall_length)
 
-    # bottom
-    pygame.draw.line(surface=screen, color=wall_color, width=wall_thickness,
-                     start_pos=(dist_l_r, dist_t_b),
-                     end_pos=(dist_l_r + wall_length, dist_t_b))
+    top_start = (dist_l_r, dist_t_b + wall_length)
+    top_end = (dist_l_r + wall_length, dist_t_b + wall_length)
 
+    right_start = (dist_l_r + wall_length, dist_t_b + wall_length)
+    right_end = (dist_l_r + wall_length, dist_t_b)
 
-def draw_robot(screen, x, y, radius, robot_color):
+    bottom_start = (dist_l_r, dist_t_b)
+    bottom_end = (dist_l_r + wall_length, dist_t_b)
 
-    # body of robot
-    pygame.draw.circle(surface=screen, color=robot_color, center=(x, y), radius=radius)
-    # orientation of robot
-    pygame.draw.line(surface=screen, color=(0, 0, 0), start_pos=(x, y), end_pos=(x + radius, y), width=2)
+    walls = {
+        'left': [left_start, left_end],
+        'top': [top_start, top_end],
+        'right': [right_start, right_end],
+        'bottom': [bottom_start, bottom_end],
+    }
 
+    return walls
 
 if __name__ == '__main__':
 
@@ -49,11 +79,14 @@ if __name__ == '__main__':
     # robot
     x = env_width / 2
     y = env_height / 2
-    v = 6
+    v = 20
     radius = env_width / 25
+    num_sensors = 12
     max_sensor_reach = 2 * radius
     robot_color = (153,204,255)
-    robot = Robot(x, y, radius, max_sensor_reach)
+    robot = Robot(x, y, radius, num_sensors, max_sensor_reach)
+
+    walls = init_walls_coordinates(env_width, env_height, wall_length)
 
     # initialize pygame
     pygame.init()
@@ -72,17 +105,25 @@ if __name__ == '__main__':
             pressed_keys = pygame.key.get_pressed()
             if pressed_keys[pygame.K_UP]:
                 robot.y -= v
+                robot.update_sensors()
             if pressed_keys[pygame.K_DOWN]:
                 robot.y += v
+                robot.update_sensors()
             if pressed_keys[pygame.K_LEFT]:
                 robot.x -= v
+                robot.update_sensors()
             if pressed_keys[pygame.K_RIGHT]:
                 robot.x += v
+                robot.update_sensors()
+
+
+            if robot.robot_is_crossing_wall(walls):
+                print("Robot bumped into wall")
 
             # clear screen
             screen.fill((255,255,255))
 
-            draw_walls(screen, env_width, env_height, wall_length, wall_thickness, wall_color)
-            draw_robot(screen, robot.x, robot.y, robot.radius, robot_color)
+            draw_walls(screen, walls, wall_thickness, wall_color)
+            draw_robot(screen, robot, robot_color, draw_sensors=True)
             pygame.display.update()
             clock.tick(60)
--- 2_mobile_robot_simulator/robot.py
+++ 2_mobile_robot_simulator/robot.py
@@ -1,7 +1,11 @@
+import math
+from shapely.geometry import LineString
+from shapely.geometry import Point
+
 
 class Robot:
 
-    def __init__(self, x, y, radius, max_sensor_reach):
+    def __init__(self, x, y, radius, num_sensors, max_sensor_reach):
         self.x = x
         self.y = y
         self.radius = radius
@@ -10,6 +14,41 @@ class Robot:
         self.v_wheel_l = 0
         self.v_wheel_r = 0
 
+        self.num_sensors = num_sensors
         self.max_sensor_reach = max_sensor_reach
         self.sensor_list = []
+        self.init_sensors()
+
+
+    def init_sensors(self):
+        self.update_sensors()
+
+    def update_sensors(self):
+        # update coordinates of sensors after robot has moved
+        sensors = []
+        for sensor_id in range(self.num_sensors):
+            angle = (sensor_id + 1) * 360 / self.num_sensors
+            length_sensor_line = self.radius + self.max_sensor_reach
+            x_sensor = self.x + length_sensor_line * math.cos(angle * math.pi / 180)
+            y_sensor = self.y + length_sensor_line * math.sin(angle * math.pi / 180)
+            sensors.append((x_sensor, y_sensor))
+
+        self.sensor_list = sensors
+
+
+    def robot_is_crossing_wall(self, walls):
+
+        for wall_name, wall_coord in walls.items():
+            position = Point(self.x, self.y)
+            circle = position.buffer(self.radius).boundary
+            line = LineString([wall_coord[0], wall_coord[1]])
+            intersection = circle.intersection(line)
+            # print(intersection)
+
+            if not intersection.is_empty:
+                return True
+
+        return False
+
+
 
