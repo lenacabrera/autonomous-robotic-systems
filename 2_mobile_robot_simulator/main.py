b9ad1ce613365cc3249e0fd03d4f048a28259d00
kathrin229 <39700827+kathrin229@users.noreply.github.com>
2021-02-16 14:59:18 +0100

    implementation of motion model

--- 2_mobile_robot_simulator/main.py
+++ 2_mobile_robot_simulator/main.py
@@ -34,22 +34,22 @@ def draw_robot(screen, x, y, radius, robot_color):
     # body of robot
     pygame.draw.circle(surface=screen, color=robot_color, center=(x, y), radius=radius)
     # orientation of robot
-    pygame.draw.line(surface=screen, color=(0, 0, 0), start_pos=(x, y), end_pos=(x + radius, y), width=2)
+    pygame.draw.line(surface=screen, color=(0, 0, 0), start_pos=(x, y), end_pos=robot.orientation, width=2)
 
 
 if __name__ == '__main__':
 
     # environment
-    env_width = 1000
+    env_width = 500
     env_height = env_width
-    wall_length = 800
+    wall_length = 400
     wall_thickness = 6
     wall_color = (204, 0, 102)
 
     # robot
     x = env_width / 2
     y = env_height / 2
-    v = 6
+    v = 0.1  #TODO: fine-tuning
     radius = env_width / 25
     max_sensor_reach = 2 * radius
     robot_color = (153,204,255)
@@ -59,6 +59,15 @@ if __name__ == '__main__':
     pygame.init()
     screen = pygame.display.set_mode([env_width, env_height])
     clock = pygame.time.Clock()
+
+    # initialize display velocity
+    myfont = pygame.font.SysFont('Arial', 14)
+    screen.fill((255, 255, 255))
+
+    # initialize timer in order to provide constant timer-events
+    timer_event = pygame.USEREVENT + 1
+    time = 250  #TODO: fine-tuning
+    pygame.time.set_timer(timer_event, time)
 
     # run animation
     go = True
@@ -70,19 +79,53 @@ if __name__ == '__main__':
                 sys.exit()
 
             pressed_keys = pygame.key.get_pressed()
-            if pressed_keys[pygame.K_UP]:
-                robot.y -= v
-            if pressed_keys[pygame.K_DOWN]:
-                robot.y += v
-            if pressed_keys[pygame.K_LEFT]:
-                robot.x -= v
-            if pressed_keys[pygame.K_RIGHT]:
-                robot.x += v
 
-            # clear screen
-            screen.fill((255,255,255))
+            # simple robot movement
+            # if pressed_keys[pygame.K_UP]:
+            #     robot.y -= v
+            # if pressed_keys[pygame.K_DOWN]:
+            #     robot.y += v
+            # if pressed_keys[pygame.K_LEFT]:
+            #     robot.x -= v
+            # if pressed_keys[pygame.K_RIGHT]:
+            #     robot.x += v
 
-            draw_walls(screen, env_width, env_height, wall_length, wall_thickness, wall_color)
-            draw_robot(screen, robot.x, robot.y, robot.radius, robot_color)
-            pygame.display.update()
-            clock.tick(60)
+            # change velocity one wheel
+            if pressed_keys[pygame.K_w]:
+                 robot.v_wheel_l +=v
+            if pressed_keys[pygame.K_s]:
+                robot.v_wheel_l -=v
+            if pressed_keys[pygame.K_o]:
+                robot.v_wheel_r +=v
+            if pressed_keys[pygame.K_l]:
+                robot.v_wheel_r -=v
+
+            # change velocity both wheels
+            if pressed_keys[pygame.K_x]:
+                robot.v_wheel_l = 0
+                robot.v_wheel_r = 0
+            if pressed_keys[pygame.K_t]:
+                robot.v_wheel_l += v
+                robot.v_wheel_r += v
+            if pressed_keys[pygame.K_g]:
+                robot.v_wheel_l -= v
+                robot.v_wheel_r -= v
+
+            # update screen by providing timer-event
+            elif event.type == timer_event:
+                robot.set_new_position(1)
+
+                # clear screen
+                screen.fill((255, 255, 255))
+
+                # print velocities
+                text = 'v_left: ' + str('%.3f'%(robot.v_wheel_l)) + '   v_right: ' + str('%.3f'%(robot.v_wheel_r))
+                textsurface = myfont.render(text, False, (0, 0, 0))
+                screen.blit(textsurface, (env_width / 3, wall_length + (env_height - wall_length)/1.5))
+
+                # draw scene
+                draw_walls(screen, env_width, env_height, wall_length, wall_thickness, wall_color)
+                draw_robot(screen, robot.x, robot.y, robot.radius, robot_color)
+
+                pygame.display.update()
+                # clock.tick(120)
--- 2_mobile_robot_simulator/robot.py
+++ 2_mobile_robot_simulator/robot.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 
 class Robot:
 
@@ -5,11 +7,65 @@ class Robot:
         self.x = x
         self.y = y
         self.radius = radius
-        self.orientation = (x + radius, y)
+        self.orientation = (x + radius, y)  # initialization: line to the right
 
         self.v_wheel_l = 0
         self.v_wheel_r = 0
 
         self.max_sensor_reach = max_sensor_reach
         self.sensor_list = []
+
+    def set_new_position(self, delta_t):
+        # # calculate angular velocity: omega
+        omega = (self.v_wheel_l - self.v_wheel_r) / 2
+
+        # # calculate distance to ICC: R
+        if self.v_wheel_l != self.v_wheel_r:
+            R = self.radius * (self.v_wheel_l + self.v_wheel_r) / (self.v_wheel_l - self.v_wheel_r)
+        else:
+            R = 100
+
+        # # calculate angle of robot relative to x-axis (horizontal axis): Theta
+        # vector_1 pointing in the direction of the x axis, vector_2 in the direction of the robot
+        vector_1 = [1, 0]
+        vector_2 = [self.orientation[0] - self.x, self.orientation[1] - self.y]
+        # normalization of the vectors
+        unit_vector_1 = vector_1 / np.linalg.norm(vector_1)
+        unit_vector_2 = vector_2 / np.linalg.norm(vector_2)
+        # dot product and determinant
+        dot = unit_vector_1[0] * unit_vector_2[0] + unit_vector_1[1] * unit_vector_2[1]
+        det = unit_vector_1[0] * unit_vector_2[1] - unit_vector_2[0] * unit_vector_1[1]
+        # the angle Theta is calculated with arctan2 as angles between 0 and 360 degrees are needed
+        Theta = np.arctan2(det, dot)
+
+        # # calculate ICC
+        ICCx = self.x - R * np.sin(Theta)
+        ICCy = self.y + R * np.cos(Theta)
+
+        # # equation (5) slide 19
+        # matrix 1
+        m1 = np.array([[np.cos(omega * delta_t), - np.sin(omega * delta_t), 0],
+                       [np.sin(omega * delta_t), np.cos(omega * delta_t), 0],
+                       [0, 0, 1]
+                       ])
+        # matrix 2
+        m2 = np.array([[self.x - ICCx],
+                       [self.y - ICCy],
+                       [Theta]
+                       ])
+        # matrix 3
+        m3 = np.array([[ICCx],
+                       [ICCy],
+                       [omega * delta_t]
+                       ])
+        # the multiplication and addition: The output is [x' y' Theta']
+        new = np.matmul(m1, m2) + m3
+
+        # # updating the x and the y coordinate of the robot
+        self.x = new[0][0]
+        self.y = new[1][0]
+
+        # # updating the orientation: the point on the border of the circle
+        self.orientation = (self.x + np.cos(new[2][0]) * self.radius,
+                            self.y + np.sin(new[2][0]) * self.radius)
 
